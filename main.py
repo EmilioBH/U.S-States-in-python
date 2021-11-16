d52c5153e29ffac0e029dec98388246b4d8ccc82
Emilio Bonmatí <emilre68@gmail.com>
2021-11-16 11:25:55 +0100

    ending the project

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import turtle
 import pandas
 
+game_on = True
+states_guessed = []
 
 sc = turtle.Screen()
 sc.title("U.S. States Game")
@@ -8,5 +10,33 @@ image = "blank_states_img.gif"
 sc.addshape(image)
 turtle.shape(image)
 
-answer_state = sc.textinput(title="Guess the State", prompt="What's another state's name? ").title()
-print(answer_state)
+data = pandas.read_csv("50_states.csv")
+print(data)
+
+while game_on:
+    answer_state = sc.textinput(title=f"{len(states_guessed)}/50 Guess the State",
+                                prompt="What's another state's name? ").title()
+    print(answer_state)
+
+    counter = 0
+    for state in data["state"]:
+        if state == answer_state and state not in states_guessed:
+            states_guessed.append(state)
+            if len(states_guessed) == 50:
+                game_on = False
+            x_cord = data["x"]
+            y_cord = data["y"]
+            new_state = turtle.Turtle()
+            new_state.penup()
+            new_state.goto(x_cord[counter], y_cord[counter])
+            new_state.write(f"{state}")
+            new_state.hideturtle()
+
+        counter += 1
+
+end_game = turtle.Turtle()
+end_game.hideturtle()
+end_game.color("red")
+end_game.write("You complete the game!! 😁", font=("Arial", 20, "normal"))
+sc.exitonclick()
+
